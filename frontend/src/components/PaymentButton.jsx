af309cafefc5c81fcdea948bd864be7b373859c7
Eminence <pteminence@gmail.com>
2026-06-18 03:04:59 -0500

    refactor: paymentbutton

--- frontend/src/components/PaymentButton.jsx
+++ frontend/src/components/PaymentButton.jsx
@@ -1,41 +1,10 @@
 import React from 'react';
-//import { useFlutterwave, closePaymentModal } from 'flutterwave-react-v3';
 
-const PaymentButton = ({ amount, email, name, onSuccess }) => {
-  const config = {
-    public_key: 'FLWPUBK-ad400c106b75234a9a6aa659ad96659a-X', // Replace with your Public Key
-    tx_ref: Date.now().toString(),
-    amount: amount,
-    currency: 'NGN', // Change if necessary
-    payment_options: 'card,banktransfer,ussd',
-    customer: {
-      email: email,
-      name: name,
-    },
-    customizations: {
-      title: 'Unlock Listing',
-      description: 'Payment for premium listing access',
-      logo: 'https://kiwi-list-ifnr.onrender.com/favicon.svg',
-    },
-  };
-
-  const handleFlutterPayment = useFlutterwave(config);
-
+const PaymentButton = ({ onUnlock }) => {
   return (
     <button
       className="bg-blue-600 text-white px-4 py-2 rounded"
-      onClick={() => {
-        handleFlutterPayment({
-          callback: (response) => {
-            console.log(response);
-            onSuccess(response); // Handle logic after success
-            closePaymentModal(); // Close the modal
-          },
-          onClose: () => {
-            console.log("Payment modal closed");
-          },
-        });
-      }}
+      onClick={onUnlock}
     >
       Pay Now
     </button>
